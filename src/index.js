8a17c96335a28597950483f8111ee1f99a2eb651
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-14 16:13:36 +0530

    broadcasting events

--- src/index.js
+++ src/index.js
@@ -13,8 +13,18 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
-    console.log('New websocket connection')
+io.on('connection', (socket) => {
+    console.log('New websocket connection');
+
+    socket.emit('message', 'Welcome');
+    socket.broadcast.emit('message', 'A new user has joined');
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
 })
 
 server.listen(port, () => {
