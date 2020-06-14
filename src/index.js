4e991c1d13fab844e7f93cb0b05fc8ae555e8355
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-14 20:07:56 +0530

    sharing location

--- src/index.js
+++ src/index.js
@@ -17,15 +17,21 @@ io.on('connection', (socket) => {
     console.log('New websocket connection');
 
     socket.emit('message', 'Welcome');
+
     socket.broadcast.emit('message', 'A new user has joined');
+
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
     });
 
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left')
-    })
-})
+    });
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    });
+});
 
 server.listen(port, () => {
     console.log(`Server is running on ${port}`)
