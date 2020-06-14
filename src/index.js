f86766e203424600f99766311b4c4aa37a079582
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-14 13:36:06 +0530

    setting up socket.io

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 const path = require('path');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('New websocket connection')
+})
+
+server.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
