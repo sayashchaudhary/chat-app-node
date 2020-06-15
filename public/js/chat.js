94335fdec43b22583877188817e3f64a8eaab3c5
sayash chaudhary <sayashchaudhary@gmail.com>
2020-06-15 23:57:48 +0530

    socket.io rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,11 @@ const $messages = document.querySelector('#messages');
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
 
+//Options
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
@@ -63,3 +68,5 @@ $sendLocationButton.addEventListener('click', () => {
         })
     })
 })
+
+socket.emit('join', { username, room })
